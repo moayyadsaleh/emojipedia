340f39ee6d291b1fc8a61afad57db8538106ed79
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-08-27 10:19:20 -0600

    solve out the problem using a different way

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import Entry from "./Entry";
 import "./styles.css";
-import EmojiList from "./EmojiList"; // Adjust the import path
+import emojipedia from "../emojipedia"; //import the emojipedia in order to map through it
+//Create Entry component
+//create props to replace hardcoded data
+// import the emojipedia const and map through emojipedia array and render Entry components
+console.log(emojipedia);
+function CreateEntry(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      describtion={emojiTerm.meaning}
+    />
+  );
+}
 
 function App() {
   return (
@@ -8,10 +23,8 @@ function App() {
       <h1>
         <span>emojipedia</span>
       </h1>
-
-      <dl className="dictionary">
-        <EmojiList /> {/* Use the EmojiList function */}
-      </dl>
+      <dl className="dictionary"></dl>
+      {emojipedia.map(CreateEntry)}
     </div>
   );
 }
